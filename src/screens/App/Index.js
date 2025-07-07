b2c2e91be02def42801e85b7253dce768faea95b
Abdul Basit <abdulrocker4@gmail.com>
2025-07-07 18:56:20 +0500

    setting flow is in progress

--- src/screens/App/Index.js
+++ src/screens/App/Index.js
@@ -2,12 +2,12 @@ import HomeScreen from "./Home/HomeScreen";
 import MarketScreen from "./Market/MarketScreen";
 import Exchangescreen from "./Exchange/Exchangescreen";
 import WalletScreen from "./Wallet/WalletScreen";
-import settingMain from "./Settings/settingMain";
 import settingProfile from "./SettingProfile/SettingProfile";
 import WalletHome from "./Deposit/WalletHome";
 import DepositHistory from "./DepositHistory/DepositHistory";
 import settingSecurity from "./SettingSecurity/SettingSecurity";
 import twoFactorAuth from "./TwoFactorAuth/TwoFactorAuth";
+import settingMain from "./Settings/SettingMain";
 
 export {
      HomeScreen, settingMain, twoFactorAuth, settingSecurity, settingProfile, WalletHome, DepositHistory, MarketScreen, Exchangescreen, WalletScreen
