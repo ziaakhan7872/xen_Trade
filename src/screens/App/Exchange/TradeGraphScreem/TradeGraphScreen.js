9ff898d5c08a411418c69f983c74d6c498eec820
Usama Nawaz <kshinwari948@gmail.com>
2025-07-24 14:25:21 +0500

    add bottom sheet in exchange screen and add order book in order screen

--- src/screens/App/Exchange/TradeGraphScreem/TradeGraphScreen.js
+++ src/screens/App/Exchange/TradeGraphScreem/TradeGraphScreen.js
@@ -1,40 +1,64 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { ScrollView, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import { ExchangeMainContainer } from '../../../../components/ExchangeMainContainer'
 import UseTradeGraphScreen from './Hooks/Index'
 import Line from '../../../../components/Liner'
-import { hp } from '../../../../components/ResponsiveComponent'
+import { hp, wp } from '../../../../components/ResponsiveComponent'
 import Spacer from '../../../../components/Spacer'
-import { CoinPriceDetail, TradeGraph, TradeGraphHeader, TradeHeader } from './Component/Index'
+import { BuySellButton, CoinPriceDetail, FavoutiteBottomSheetComponnet, FlatlistValues, OrderBookHeader, PriceUSDT, TradeGraph, TradeGraphBelowHeader, TradeGraphHeader, TradeHeader } from './Component/Index'
+import { Amount } from '../../../../utilities/dummyData'
+import { colors } from '../../../../constants'
+import RowButton from '../../../../components/RowButton'
+import { Portal } from 'react-native-portalize'
 
 const TradeGraphScreen = (props) => {
-    const { starPress, setStarPress,candleChartData,setCandleChartData } = UseTradeGraphScreen(props)
-    // const data = [
-    //     { timestamp: 1625945400000, open: 0.15, high: 0.18, low: 0.12, close: 0.16 },
-    //     { timestamp: 1625949000000, open: 0.16, high: 0.17, low: 0.14, close: 0.15 },
-    //     { timestamp: 1625952600000, open: 0.16, high: 0.19, low: 0.15, close: 0.18 },
-    //     { timestamp: 1625956200000, open: 0.14, high: 0.17, low: 0.13, close: 0.15 },
-    //     { timestamp: 1625956200000, open: 0.14, high: 0.17, low: 0.13, close: 0.15 },
-    //     { timestamp: 1625956200000, open: 0.14, high: 0.17, low: 0.13, close: 0.15 },
-    //     { timestamp: 1625956200000, open: 0.14, high: 0.17, low: 0.13, close: 0.15 },
-    //     { timestamp: 1625956200000, open: 0.14, high: 0.17, low: 0.13, close: 0.15 },
-    //     { timestamp: 1625956200000, open: 0.14, high: 0.17, low: 0.13, close: 0.15 },
+    const { starPress, setStarPress, candleChartData, setCandleChartData, orderBookHeaderPress, setOrderBookHeaderPress,favouriteBottomSheetRef } = UseTradeGraphScreen(props)
 
-    // ];
 
 
     return (
         <ExchangeMainContainer>
-            <TradeHeader onBackPress={() => props?.navigation?.goBack()} setStarPress={setStarPress} starPress={starPress} />
-            <Spacer />
-            <Line height={hp(0.1)} />
-            <CoinPriceDetail />
-            <Spacer />
-            <TradeGraphHeader />
-            <Spacer />
-            <Line height={hp(0.1)} />
-            <Spacer />
-            <TradeGraph data={candleChartData} />
+            <TradeHeader onpress={()=>favouriteBottomSheetRef?.current?.expand()} onBackPress={() => props?.navigation?.goBack()} setStarPress={setStarPress} starPress={starPress} />
+            <ScrollView contentContainerStyle={{ paddingBottom:hp(6) }}>
+                <Spacer />
+                <Line height={hp(0.1)} />
+                <CoinPriceDetail />
+                <Spacer />
+                <TradeGraphHeader />
+                <Spacer />
+                <Line height={hp(0.1)} />
+                <Spacer />
+                <TradeGraph data={candleChartData} />
+                <Spacer />
+                <TradeGraphBelowHeader />
+                <Spacer />
+                <Line width={wp(100)} height={hp(0.1)} />
+                <Spacer />
+                <OrderBookHeader buttonPress={orderBookHeaderPress} setButtonPress={setOrderBookHeaderPress} />
+                <Line width={wp(100)} height={hp(0.1)} />
+                <Spacer />
+                <PriceUSDT title1={'Price'} title2={`(${'USDT'})`} title3={'Amount'} title4={`(${'ETH'})`} />
+                <Spacer />
+                <Line width={wp(100)} height={hp(0.1)} />
+                <FlatlistValues data={Amount} textColor={colors.green} />
+                {/* <Spacer height={hp(5)}/> */}
+            </ScrollView>
+            <Portal>
+                <View style={{
+                position: "absolute",
+                paddingVertical:hp(2),
+                bottom: 5,
+                width: "100%",
+                alignItems: "center",
+                backgroundColor:colors.cardsBgColor
+            }}>
+                <BuySellButton />
+            </View>
+            <FavoutiteBottomSheetComponnet ref={favouriteBottomSheetRef}/>
+            </Portal>
+            
+
+
         </ExchangeMainContainer>
     )
 }
