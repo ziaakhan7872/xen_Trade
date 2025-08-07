d962a7fc2f46d24757902a44239b544b9e90dbc5
Usama Nawaz <kshinwari948@gmail.com>
2025-08-07 10:38:54 +0500

    did some thing

--- src/navigation/AuthNavigation/AuthNavigation.js
+++ src/navigation/AuthNavigation/AuthNavigation.js
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { Routes } from '../../constants';
 import * as Auth from '../../screens/Auth'
+import { Platform } from 'react-native';
 const { Navigator, Screen } = createNativeStackNavigator();
 
 const AuthNavigation = (props) => {
@@ -13,7 +14,10 @@ const AuthNavigation = (props) => {
 
     return (
         <Navigator screenOptions={{ headerShown: false }}>
-            <Screen name={Routes.splashScreen} component={Auth.splashScreen} />
+            {Platform.OS ==="ios" &&
+                        <Screen name={Routes.splashScreen} component={Auth.splashScreen} />
+
+            }
             <Screen name={Routes.IntroductMainScreem} component={Auth.IntroductionMain} />
             <Screen name={Routes.LoginScreen} component={Auth.LoginScreen} />
             <Screen name={Routes.SignupScreen} component={Auth.SignupScreen} />
