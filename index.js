7b0d7aedd8b006157d6b57d536b56481136f2e57
Abdul Basit <abdulrocker4@gmail.com>
2025-08-12 10:41:05 +0500

    Update index.js

--- index.js
+++ index.js
@@ -8,4 +8,7 @@ import { name as appName } from './app.json';
 import "react-native-gesture-handler"
 import "react-native-reanimated"
 
+
+// fhgjkl
+
 AppRegistry.registerComponent(appName, () => App);
