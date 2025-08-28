9a6960f92ce28c7632a1bc55cf837d1b0dea55db
Abdul Basit <abdulrocker4@gmail.com>
2025-08-28 13:23:21 +0500

    menuScreen routes added + fixed

--- src/screens/App/MenuScreen/MenuScreen.js
+++ src/screens/App/MenuScreen/MenuScreen.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import { Image, Platform, ScrollView, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
-import { SafeAreaView } from 'react-native-safe-area-context' // ✅ Make sure you're using this one
+import { Image, Platform, ScrollView, TouchableOpacity, View } from 'react-native'
+import { SafeAreaView } from 'react-native-safe-area-context'
 import { style } from './Style'
 import Entypo from "react-native-vector-icons/Entypo"
 import { colors, Routes } from '../../../constants'
@@ -10,15 +10,13 @@ import Spacer, { HorizontalSpacer } from '../../../components/Spacer'
 import { ResponsiveText } from '../../../components/ResponsiveText'
 import Line from '../../../components/Liner'
 import { appStyles } from '../../../utilities'
-import { navigate } from '../../../navigation/NavigationService/NavigationService'
 import { LogoutUser } from '../../../constants/Api/Index'
 import { getRefreshToken } from '../../../redux/store'
-import { useDispatch } from 'react-redux'
 import { UseMenuScreen } from './Hooks/Index'
 
 const MenuScreen = (props) => {
-    const {logout} = UseMenuScreen(props)
-   
+    const { logout } = UseMenuScreen(props)
+
     return (
         <SafeAreaView style={{ flex: 1, backgroundColor: colors.black, paddingTop: Platform.OS === 'android' ? hp(2.5) : 0 }} >
             <View style={style.container}>
@@ -39,7 +37,7 @@ const MenuScreen = (props) => {
                         <ResponsiveText style={style.label}>Exchange</ResponsiveText>
                     </TouchableOpacity>
                     <Spacer />
-                    <TouchableOpacity onPress={() => props?.navigation.navigate(Routes.AppNavigator, { screen: Routes.referrals })} style={{ flexDirection: "row", alignItems: "center" }}>
+                    <TouchableOpacity onPress={() => props?.navigation.navigate(Routes.referrals)} style={{ flexDirection: "row", alignItems: "center" }}>
                         <Image style={style.imageStyling} source={images.addressSettingIcon} resizeMode='contain' />
                         <HorizontalSpacer width={wp(3)} />
                         <ResponsiveText style={style.label}>Referrals</ResponsiveText>
@@ -53,7 +51,7 @@ const MenuScreen = (props) => {
                         <ResponsiveText style={style.label}>Markets</ResponsiveText>
                     </TouchableOpacity>
                     <Spacer />
-                    <TouchableOpacity style={{ flexDirection: "row", alignItems: "center" }}>
+                    <TouchableOpacity style={{ flexDirection: "row", alignItems: "center" }} onPress={() => props?.navigation?.navigate?.(Routes.addressBook)}>
                         <Image style={style.imageStyling} source={images.Address} resizeMode='contain' />
                         <HorizontalSpacer width={wp(3)} />
                         <ResponsiveText style={style.label}>Address Book</ResponsiveText>
@@ -61,13 +59,13 @@ const MenuScreen = (props) => {
                     <Spacer />
                     <Line height={hp(0.1)} width={wp(90)} />
                     <Spacer />
-                    <TouchableOpacity style={{ flexDirection: "row", alignItems: "center" }}>
+                    <TouchableOpacity style={{ flexDirection: "row", alignItems: "center" }} onPress={() => props?.navigation?.navigate?.(Routes.settingProfile)}>
                         <Image style={style.imageStyling} source={images.profileSettingIcon} resizeMode='contain' />
                         <HorizontalSpacer width={wp(3)} />
                         <ResponsiveText style={style.label}>Profile</ResponsiveText>
                     </TouchableOpacity>
                     <Spacer />
-                    <TouchableOpacity style={{ flexDirection: "row", alignItems: "center" }}>
+                    <TouchableOpacity style={{ flexDirection: "row", alignItems: "center" }} onPress={() => props?.navigation?.navigate?.(Routes.settingSecurity)}>
                         <Image style={style.imageStyling} source={images.securitySettingIcon} resizeMode='contain' />
                         <HorizontalSpacer width={wp(3)} />
                         <ResponsiveText style={style.label}>Security</ResponsiveText>
@@ -79,13 +77,13 @@ const MenuScreen = (props) => {
                         <ResponsiveText style={style.label}>Verification</ResponsiveText>
                     </TouchableOpacity>
                     <Spacer />
-                    <TouchableOpacity style={{ flexDirection: "row", alignItems: "center" }}>
+                    <TouchableOpacity style={{ flexDirection: "row", alignItems: "center" }} onPress={() => props?.navigation?.navigate?.(Routes.settingEmailNotification)} >
                         <Image style={style.imageStyling} source={images.emailNotificationSettingIcon} resizeMode='contain' />
                         <HorizontalSpacer width={wp(3)} />
                         <ResponsiveText style={style.label}>Notifications</ResponsiveText>
                     </TouchableOpacity>
                     <Spacer />
-                    <TouchableOpacity style={{ flexDirection: "row", alignItems: "center" }}>
+                    <TouchableOpacity style={{ flexDirection: "row", alignItems: "center" }} onPress={() => props?.navigation?.navigate?.(Routes.settingSystem)} >
                         <Image style={style.imageStyling} source={images.systemSettingIcon} resizeMode='contain' />
                         <HorizontalSpacer width={wp(3)} />
                         <ResponsiveText style={style.label}>System Settings</ResponsiveText>
@@ -94,7 +92,7 @@ const MenuScreen = (props) => {
                     <Spacer />
                     <Line height={hp(0.1)} width={wp(90)} />
                     <Spacer />
-                    <TouchableOpacity style={appStyles.rowBasic} onPress={() => props?.navigation.navigate(Routes.AppNavigator, { screen: Routes.ContactSupport })}>
+                    <TouchableOpacity style={appStyles.rowBasic} onPress={() => props?.navigation?.navigate(Routes.ContactSupport)}>
                         <Image style={style.imageStyling} source={images.Contact} resizeMode='contain' />
                         <HorizontalSpacer width={wp(3)} />
                         <ResponsiveText style={style.label}>Contact Support</ResponsiveText>
