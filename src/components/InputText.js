24211fbf729acb0a300d54f8dc90f6fc75e10de7
Abdul Basit <abdulrocker4@gmail.com>
2025-08-29 16:42:23 +0500

    Update InputText.js

--- src/components/InputText.js
+++ src/components/InputText.js
@@ -5,7 +5,6 @@ import { hp, wp } from './ResponsiveComponent'
 import Feather from 'react-native-vector-icons/Feather';
 import EvilIcons from "react-native-vector-icons/EvilIcons"
 
-
 const InputText = ({
     onFocus, onBlur,
     handleRightIconPress,
